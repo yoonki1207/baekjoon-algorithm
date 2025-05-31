5c2e2a9387451bc0b40ed975c1adf7d564aa21a1
Yoonki Hong <yoonki1207@gmail.com>
2025-05-31 18:59:47 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <math.h>
+#include <map>
+#include <set>
+#include <string>
+#define INF 987654321
+#define lld long long
+#define MAX_N 1000001
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+cout << "  ___  ___  ___\n";
+cout << "  | |__| |__| |\n";
+cout << "  |           |\n";
+cout << "   \\_________/\n";
+cout << "    \\_______/\n";
+cout << "     |     |\n";
+cout << "     |     |\n";
+cout << "     |     |\n";
+cout << "     |     |\n";
+cout << "     |_____|\n";
+cout << "  __/       \\__\n";
+cout << " /             \\\n";
+cout << "/_______________\\\n";
+	return 0;
+}
