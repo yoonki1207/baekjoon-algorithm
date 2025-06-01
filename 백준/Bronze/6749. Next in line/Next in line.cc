4e4d20f24d01190fedf81aa72f54019c4c005d0f
Yoonki Hong <yoonki1207@gmail.com>
2025-06-01 21:17:02 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <math.h>
+#include <map>
+#include <set>
+#include <string>
+#define INF 987654321
+#define lld long long
+#define MAX_N 1000001
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int a,b;
+	cin >> a;
+	cin >> b;
+	cout << (b-a+b);
+	return 0;
+}
