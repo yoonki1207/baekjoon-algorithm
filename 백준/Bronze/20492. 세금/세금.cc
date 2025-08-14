22c896684bb22382806fe37b98cb3cc119bdc3a1
Yoonki Hong <yoonki1207@gmail.com>
2025-08-14 18:35:01 +0900

    [Bronze V] Title: 세금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <math.h>
+#include <map>
+#include <climits>
+#include <set>
+#include <string>
+#define INF 987654321
+#define MAX_N 65537
+#define lld long long int
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.tie(NULL);
+	int n; cin >> n;
+	cout << (int)(n*0.78) << ' ' << (int)(n*0.956);
+	return 0;
+}
+// 10000000000000 2 2 0 0
