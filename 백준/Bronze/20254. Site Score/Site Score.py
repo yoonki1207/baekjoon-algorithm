e535abcc176741fe1a9268054a3bfb5485f7c2b4
Yoonki Hong <yoonki1207@gmail.com>
2025-08-30 22:18:32 +0900

    [Bronze V] Title: Site Score, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.py
@@ -0,0 +1,4 @@
+# 56UR + 24TR + 14UO + 6TO
+arr = input().split(' ')
+arr[0], arr[1], arr[2], arr[3] = int(arr[0]), int(arr[1]), int(arr[2]), int(arr[3])
+print(arr[0]*56 + arr[1]*24 + arr[2]*14 + arr[3]*6)
