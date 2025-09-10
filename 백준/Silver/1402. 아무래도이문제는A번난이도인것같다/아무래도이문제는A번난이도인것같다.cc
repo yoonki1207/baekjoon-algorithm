59194451322f0c54414d7739054f75b624101ffb
Yoonki Hong <yoonki1207@gmail.com>
2025-09-10 18:39:42 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <math.h>
+#include <map>
+#include <climits>
+#include <set>
+#include <string>
+#include <string.h>
+#define INF 987654321
+#define MAX_N 102
+#define lld long long int
+#define pii pair<int, int>
+// #define DEBUG
+
+using namespace std;
+
+int T, A, B;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.tie(NULL);
+	cin >> T;
+	for(int test_case = 1; test_case <= T; test_case++) {
+		cin >> A >> B;
+		cout << "yes\n";
+	}
+	
+	return 0;
+}
