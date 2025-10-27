ef932d38498b63d66b206eebc827a5305125229b
Yoonki Hong <yoonki1207@gmail.com>
2025-10-27 15:47:45 +0900

    [Silver III] Title: 낚이고 낚아라, Time: 356 ms, Memory: 3688 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15821. 낚이고 낚아라/낚이고 낚아라.cc
+++ 백준/Silver/15821. 낚이고 낚아라/낚이고 낚아라.cc
@@ -21,7 +21,7 @@ using namespace std;
 
 int K, N, P;
 
-lld squred_dist(int x, int y) {
+lld squred_dist(lld x, lld y) {
     return x*x + y*y;
 }
 
@@ -33,7 +33,7 @@ int main() {
     vector<long long> v;
     for(int i = 0; i < N; i++) {
         cin >> P;
-        int x, y;
+        lld x, y;
         lld max_dist = 0;
         for(int j = 0; j < P; j++) {
             cin >> x >> y;
