5b482bb18441686078fee272f65d1f7b02216665
홍윤기 <yoonki1207@gmail.com>
2026-03-13 15:38:18 +0900

    [Silver II] Title: 색종이 만들기, Time: 128 ms, Memory: 111480 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,9 +1,6 @@
 # 분할정복 - 색종이 만들기 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/2630
 
-BLUE = 1
-WHITE = 0
-
 N = int(input())
 arr = [[0 for _ in range(N)] for _ in range(N)]
 for i in range(N):
@@ -11,29 +8,32 @@ for i in range(N):
     for j in range(N):
         arr[i][j] = line[j]
 
-
-def checkAll(y: int, x: int, n: int, color: int):
-    for i in range(y, y + n):
-        for j in range(x, x + n):
+def isAllColor(y, x, m, color):
+    for i in range(y, y + m):
+        for j in range(x, x + m):
             if arr[i][j] != color:
                 return False
     return True
 
-colorcnt = [0, 0]
+white_cnt = 0 # white is 0
+blue_cnt = 0 # blue is 1
+WHITE = 0
+BLUE = 1
 
-def solve(y, x, n):
-    allBlue = checkAll(y, x, n, BLUE)
-    allWhite = checkAll(y, x, n, WHITE)
-    if allBlue:
-        colorcnt[BLUE] += 1
-    elif allWhite:
-        colorcnt[WHITE] += 1
-    else:
-        m = n//2
-        solve(y, x, m)
-        solve(y+m, x, m)
-        solve(y, x+m, m)
-        solve(y+m, x+m, m)
-solve(0, 0, N)
-for i in colorcnt:
-    print(i)
+def cutPaper(y, x, m):
+    global blue_cnt, white_cnt
+    if isAllColor(y, x, m, WHITE):
+        white_cnt += 1
+        return
+    if isAllColor(y, x, m, BLUE): 
+        blue_cnt += 1
+        return
+    m //= 2
+    cutPaper(y, x, m)
+    cutPaper(y+m, x, m)
+    cutPaper(y, x+m, m)
+    cutPaper(y+m, x+m, m)
+
+cutPaper(0, 0, N)
+print(white_cnt)
+print(blue_cnt)
