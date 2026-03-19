b4e2128420e515c4a5688bfead869e9b6a4a0130
홍윤기 <yoonki1207@gmail.com>
2026-03-19 22:26:57 +0900

    [Silver III] Title: 바이러스, Time: 112 ms, Memory: 112924 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,23 @@
+N = int(input())
+M = int(input())
+arr = [[False for _ in range(N + 1)] for _ in range(N + 1)]
+for i in range(M):
+    a, b = map(int, input().split())
+    arr[a][b] = arr[b][a] = True
+
+visited = [False for _ in range(N + 1)]
+
+from collections import deque
+q = deque([1])
+visited[1] = True
+cnt = 0
+
+while q:
+    front = q.popleft()
+    cnt += 1
+    for index, isCon in enumerate(arr[front]):
+        if isCon and not visited[index]:
+            visited[index] = True
+            q.append(index)
+
+print (cnt - 1)
